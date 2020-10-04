10757fb4f92ddcc17cc85d807a990a4557bc989b
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-10-04 12:44:26 -0500

    addded menu options and a new db bug

--- database.py
+++ database.py
@@ -4,6 +4,8 @@ database = 'art.sqlite'
 
 
 def create_artist_table():
+
+
     with sqlite3.connect(database) as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS artists(\
             artistId INTEGER PRIMARY KEY,\
@@ -11,20 +13,33 @@ def create_artist_table():
             email TEXT UNIQUE NOT NULL)")
     conn.close()
 
+        
+
 def create_artwork_table():
     with sqlite3.connect(database) as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS artwork(\
-            FOREIGN KEY(workArtist) REFERENCES astists(arstistId),\
             name TEXT UNIQUE NOT NULL, \
-            price INT NOT NULL,\
-            available INT NOT NULL)")
+            price INTEGER NOT NULL,\
+            available INTEGER NOT NULL,\
+            workArtist INTEGER,\
+            FOREIGN KEY(workArtist) REFERENCES astists(arstistId))")
     conn.close()
 
 
 def add_artist(name, email):
     # todo 
-    with sqlite3.connect(database) as con: 
-        con.execute('')
+    try:
+        sql = 'INSERT INTO artists (name , email) VALUES (?,?)'
+        with sqlite3.connect(database) as con: 
+            con.execute(sql,(name,email))
+        con.close()
+    except sqlite3.IntegrityError as e:
+        raise ArtError('Sorry aritst already exists in the database.') from e
+    finally:
+        con.close()
 
 
 # todo other DB interaction
+
+class ArtError(Exception):
+    pass
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import ui 
 import database 
+from artists import Artist, Artwork
 
 def main():
+    print_menu()
+    database.create_artist_table()
+    database.create_artwork_table()
     while True:
         # what does the user want to do?
         # print menu
@@ -11,21 +15,41 @@ def main():
             add_artist()
         elif task == '2':
             add_artwork()
+        elif task == '3':
+            # search_by_artist()
+        elif task == '4':
+            # search_available_by_artist()
+        elif task == '5':
+            # change_availability()
+        elif task == '6':
+            # delete_art()
+        elif task.upper() == 'Q':
+            break
         # etc... 
 
+def print_menu():
+
+    print('1: New Artist')
+    print('2: Add new artwork')
+    print('3: Search artwork by artist')
+    print('4: Search available artworks by artist')
+    print('5: Change art availability')
+    print('6: Delete artwork')
+    print('Q: Quit')
 
 def add_artist():
     # get input 
     name = ui.get_non_empty_string('Artist name?')
     email = ui.get_non_empty_string('Artist email?')
+    
     database.add_artist(name, email)
 
 def add_artwork():
-    #artist
+    artist = ui.get_non_empty_string('Enter Artist name": ')
     name_of_artwork = ui.get_non_empty_string('Name of artwork? ')
     price = ui.get_positive_float('What is the price? ')
     avialability = ui.get_true_or_false('Is the artwork available?')
-    pass 
+     
 
 if __name__ == "__main__":
     main()
